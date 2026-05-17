d4e7abdca4744fd644aa7c95b9431871430679e0
Johnson Hu <johnson.wq.hu@gmail.com>
2026-05-17 20:57:15 +0200

    feat(broot): support transparency

--- modules/home-manager/generic/broot.nix
+++ modules/home-manager/generic/broot.nix
@@ -7,6 +7,7 @@ let
   hasSpecific = builtins.pathExists (targetPath + "/${colorscheme}.nix");
   enable = cfg.enable && cfg.${target}.enable && !hasSpecific;
   inherit (cfg.palette) ansi;
+  bg = if cfg.transparent then "none" else ansi.bg;
 in
 {
   config = lib.mkIf enable {
@@ -14,47 +15,47 @@ in
       imports = [ ];
 
       skin = {
-        default = "${ansi.fg} ${ansi.bg}";
-        tree = "${ansi.bright_black} ${ansi.bg}";
-        parent = "${ansi.bright_black} ${ansi.bg}";
-        file = "${ansi.fg} ${ansi.bg}";
-        directory = "${ansi.blue} ${ansi.bg} Bold";
-        exe = "${ansi.green} ${ansi.bg}";
-        link = "${ansi.cyan} ${ansi.bg}";
-        pruning = "${ansi.bright_black} ${ansi.bg} Italic";
-        perm__ = "${ansi.bright_black} ${ansi.bg}";
-        perm_r = "${ansi.cyan} ${ansi.bg}";
-        perm_w = "${ansi.yellow} ${ansi.bg}";
-        perm_x = "${ansi.green} ${ansi.bg}";
-        owner = "${ansi.bright_white} ${ansi.bg}";
-        group = "${ansi.magenta} ${ansi.bg}";
-        count = "${ansi.yellow} ${ansi.bg}";
-        dates = "${ansi.cyan} ${ansi.bg}";
-        sparse = "${ansi.yellow} ${ansi.bg}";
-        content_extract = "${ansi.cyan} ${ansi.bg}";
-        content_match = "${ansi.green} ${ansi.bg}";
-        device_id_major = "${ansi.bright_white} ${ansi.bg}";
-        device_id_sep = "${ansi.bright_black} ${ansi.bg}";
-        device_id_minor = "${ansi.bright_white} ${ansi.bg}";
+        default = "${ansi.fg} ${bg}";
+        tree = "${ansi.bright_black} ${bg}";
+        parent = "${ansi.bright_black} ${bg}";
+        file = "${ansi.fg} ${bg}";
+        directory = "${ansi.blue} ${bg} Bold";
+        exe = "${ansi.green} ${bg}";
+        link = "${ansi.cyan} ${bg}";
+        pruning = "${ansi.bright_black} ${bg} Italic";
+        perm__ = "${ansi.bright_black} ${bg}";
+        perm_r = "${ansi.cyan} ${bg}";
+        perm_w = "${ansi.yellow} ${bg}";
+        perm_x = "${ansi.green} ${bg}";
+        owner = "${ansi.bright_white} ${bg}";
+        group = "${ansi.magenta} ${bg}";
+        count = "${ansi.yellow} ${bg}";
+        dates = "${ansi.cyan} ${bg}";
+        sparse = "${ansi.yellow} ${bg}";
+        content_extract = "${ansi.cyan} ${bg}";
+        content_match = "${ansi.green} ${bg}";
+        device_id_major = "${ansi.bright_white} ${bg}";
+        device_id_sep = "${ansi.bright_black} ${bg}";
+        device_id_minor = "${ansi.bright_white} ${bg}";
 
-        git_branch = "${ansi.magenta} ${ansi.bg}";
-        git_insertions = "${ansi.green} ${ansi.bg}";
-        git_deletions = "${ansi.red} ${ansi.bg}";
-        git_status_current = "${ansi.fg} ${ansi.bg}";
-        git_status_modified = "${ansi.yellow} ${ansi.bg}";
-        git_status_new = "${ansi.cyan} ${ansi.bg} Bold";
-        git_status_ignored = "${ansi.bright_black} ${ansi.bg}";
-        git_status_conflicted = "${ansi.red} ${ansi.bg}";
-        git_status_other = "${ansi.red} ${ansi.bg}";
+        git_branch = "${ansi.magenta} ${bg}";
+        git_insertions = "${ansi.green} ${bg}";
+        git_deletions = "${ansi.red} ${bg}";
+        git_status_current = "${ansi.fg} ${bg}";
+        git_status_modified = "${ansi.yellow} ${bg}";
+        git_status_new = "${ansi.cyan} ${bg} Bold";
+        git_status_ignored = "${ansi.bright_black} ${bg}";
+        git_status_conflicted = "${ansi.red} ${bg}";
+        git_status_other = "${ansi.red} ${bg}";
 
         selected_line = "none ${ansi.black}";
-        char_match = "${ansi.green} ${ansi.bg} Bold";
-        file_error = "${ansi.red} ${ansi.bg}";
+        char_match = "${ansi.green} ${bg} Bold";
+        file_error = "${ansi.red} ${bg}";
 
-        flag_label = "${ansi.fg} ${ansi.bg}";
-        flag_value = "${ansi.blue} ${ansi.bg} Bold";
+        flag_label = "${ansi.fg} ${bg}";
+        flag_value = "${ansi.blue} ${bg} Bold";
 
-        input = "${ansi.fg} ${ansi.bg}";
+        input = "${ansi.fg} ${bg}";
 
         status_error = "${ansi.bright_black} ${ansi.black}";
         status_job = "${ansi.yellow} ${ansi.black}";
@@ -69,29 +70,29 @@ in
         purpose_bold = "${ansi.yellow} ${ansi.black} Bold";
         purpose_ellipsis = "${ansi.fg} ${ansi.black}";
 
-        scrollbar_track = "${ansi.bright_black} ${ansi.bg}";
-        scrollbar_thumb = "${ansi.black} ${ansi.bg}";
+        scrollbar_track = "${ansi.bright_black} ${bg}";
+        scrollbar_thumb = "${ansi.black} ${bg}";
 
-        help_paragraph = "${ansi.fg} ${ansi.bg}";
-        help_bold = "${ansi.magenta} ${ansi.bg} Bold";
-        help_italic = "${ansi.red} ${ansi.bg} Italic";
+        help_paragraph = "${ansi.fg} ${bg}";
+        help_bold = "${ansi.magenta} ${bg} Bold";
+        help_italic = "${ansi.red} ${bg} Italic";
         help_code = "${ansi.fg} ${ansi.bright_black}";
-        help_headers = "${ansi.magenta} ${ansi.bg}";
-        help_table_border = "${ansi.bright_black} ${ansi.bg}";
+        help_headers = "${ansi.magenta} ${bg}";
+        help_table_border = "${ansi.bright_black} ${bg}";
 
-        preview_title = "${ansi.fg} ${ansi.bg}";
+        preview_title = "${ansi.fg} ${bg}";
         preview = "${ansi.fg} ${ansi.black}";
-        preview_separator = "${ansi.magenta} ${ansi.bg}";
+        preview_separator = "${ansi.magenta} ${bg}";
         preview_line_number = "${ansi.fg} ${ansi.black}";
         preview_match = "none ${ansi.green}";
 
-        hex_null = "${ansi.bright_black} ${ansi.bg}";
-        hex_ascii_graphic = "${ansi.black} ${ansi.bg}";
-        hex_ascii_whitespace = "${ansi.yellow} ${ansi.bg}";
-        hex_ascii_other = "${ansi.yellow} ${ansi.bg}";
-        hex_non_ascii = "${ansi.red} ${ansi.bg}";
+        hex_null = "${ansi.bright_black} ${bg}";
+        hex_ascii_graphic = "${ansi.black} ${bg}";
+        hex_ascii_whitespace = "${ansi.yellow} ${bg}";
+        hex_ascii_other = "${ansi.yellow} ${bg}";
+        hex_non_ascii = "${ansi.red} ${bg}";
 
-        staging_area_title = "${ansi.fg} ${ansi.bg}";
+        staging_area_title = "${ansi.fg} ${bg}";
         mode_command_mark = "${ansi.bright_black} ${ansi.red} Bold";
 
         good_to_bad_0 = ansi.green;
