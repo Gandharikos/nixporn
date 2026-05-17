9c9c746d84161d22892167532bc42e5de85d8a1a
Johnson Hu <johnson.wq.hu@gmail.com>
2026-05-17 20:57:11 +0200

    feat(aerc): support transparency

--- modules/home-manager/aerc/tokyonight.nix
+++ modules/home-manager/aerc/tokyonight.nix
@@ -7,15 +7,27 @@
 let
   cfg = config.nixporn;
   inherit (cfg.colorschemes) tokyonight;
-  inherit (tokyonight) slug;
+  inherit (tokyonight) palette slug;
   source = pkgs.nixporn.tokyonight;
   target = "aerc";
   enable = cfg.enable && cfg.colorscheme == "tokyonight" && cfg.${target}.enable;
+  styleset = lib.fileContents "${source}/extras/aerc/${slug}.ini";
+  transparentStyleset =
+    lib.replaceStrings
+      [
+        palette.bg
+        palette.bg_dark
+      ]
+      [
+        "default"
+        "default"
+      ]
+      styleset;
 in
 {
   config = lib.mkIf enable {
     programs.aerc = {
-      stylesets.${slug} = lib.fileContents "${source}/extras/aerc/${slug}.ini";
+      stylesets.${slug} = if cfg.transparent then transparentStyleset else styleset;
       extraConfig.ui.styleset-name = slug;
     };
   };
